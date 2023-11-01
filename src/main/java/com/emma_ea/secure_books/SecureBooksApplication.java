f7c143a5626b7a385a919036f772a34b29fd1518
Emmanuel Awuah <emmanuelawuah.dev@gmail.com>
2023-11-01 22:20:45 +0000

    initial commit

---
+++ src/main/java/com/emma_ea/secure_books/SecureBooksApplication.java
@@ -0,0 +1,13 @@
+package com.emma_ea.secure_books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emma_ea/secure_books/SecureBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emma_ea.secure_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
